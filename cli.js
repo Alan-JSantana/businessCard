caad86513f44e27c1fda8dad4bb6b66d3b9e2e39
Alan Santana <ziggy.poeira.estelar@gmail.com>
2022-05-21 18:08:33 -0300

    created_cad

---
+++ cli.js
@@ -0,0 +1,9 @@
+var colors = require('colors');
+
+
+console.log('Name:'.bold.black.bgGreen,'Alan Carvalho Santana');
+console.log('About:'.bold.black.bgRed, 'Um mecânico aprendendo a programar, \n       Estudando Node.js, React.js e outras coisas mais!')
+console.log('Urls:'.bold.black.bgWhite,'Aqui estão minhas redes sociais e meus projetos! https://alancsantana.framer.website/ \n')
+console.log('I have a dream! (★ ω ★) \n'.rainbow, 'O meu maior sonho é criar uma escola de programação na minha comunidade!')
+
+ 
